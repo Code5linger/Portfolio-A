be9ecff56f8876c22bfef84d74b0280c8e10396b
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-08-22 03:34:42 +0600

    I am giving up for now

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,18 @@ const IS_DEVELOPMENT = process.env.NODE_ENV === "dev";
 const dirApp = path.join(__dirname, "app");
 const dirAssets = path.join(__dirname, "assets");
 const dirStyles = path.join(__dirname, "styles");
-
+const dirNode = "node_modules";
 //console.log(dirApp, dirAssets, dirStyles);
 module.exports = {
-  entry: [path.join(dirApp, "index.js")],
+  entry: [path.join(dirApp, "index.js"), path.join(dirStyles, "index.scss")],
+
+  resolve: {
+    module: [dirApp, dirAssets, dirStyles, dirNode],
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEVELOPMENT,
+    }),
+  ],
 };
